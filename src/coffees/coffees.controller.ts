210e749fffb139cf776e7c0f1ac6ee06c8229060
tklasnakov <t.klasnakov@gmail.com>
2026-01-13 06:59:08 +0200

    fix-identation

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,42 +1,42 @@
-import { 
-    Controller,
-    Get,
-    Param,
-    Post,
-    Body,
-    Patch,
-    Delete,
-    Query,
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entity/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
-    constructor(private readonly coffeesService: CoffeesService) {}
+  constructor(private readonly coffeesService: CoffeesService) {}
 
-    @Get()
-    findAll(@Query() params) {
-        return this.coffeesService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.coffeesService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.coffeesService.findOne(Number(id));
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(Number(id));
+  }
 
-    @Post()
-    create(@Body() createCoffeeDto: Coffee) {
-        return this.coffeesService.create(createCoffeeDto);
-    }
+  @Post()
+  create(@Body() createCoffeeDto: Coffee) {
+    return this.coffeesService.create(createCoffeeDto);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateCoffeeDto) {
-        return this.coffeesService.update(Number(id), updateCoffeeDto);
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto) {
+    return this.coffeesService.update(Number(id), updateCoffeeDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.coffeesService.delete(Number(id));
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.delete(Number(id));
+  }
 }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -3,44 +3,44 @@ import { Coffee } from './entity/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-    private _coffees: Coffee[] = [
-        {
-            id: 1,
-            name: 'Coffee 1',
-            brand: 'Brand 1',
-            flavors: ['flavor 1', 'flavor 2'],
-        }
-    ]
+  private _coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Coffee 1',
+      brand: 'Brand 1',
+      flavors: ['flavor 1', 'flavor 2'],
+    },
+  ];
 
-    findAll() {
-        return this._coffees;
-    }
+  findAll(): Coffee[] {
+    return this._coffees;
+  }
 
-    findOne(id: number) {
-        return this._coffees.find(coffee => coffee.id === id);
-    }
+  findOne(id: number): Coffee | undefined {
+    return this._coffees.find((coffee) => coffee.id === id);
+  }
 
-    create(coffee: Coffee) {
-        this._coffees.push(coffee);
+  create(coffee: Coffee): string {
+    this._coffees.push(coffee);
 
-        return `${coffee.name} created`;
-    }
+    return `${coffee.name} created`;
+  }
 
-    update(id: number, coffee: Coffee) {
-        this._coffees = Object.keys(this._coffees).map(key => {
-            if (this._coffees[key].id !== id) {
-                return this._coffees[key];
-            }
+  update(id: number, coffee: Coffee): Coffee[] {
+    this._coffees = this._coffees.map((item) => {
+      if (item.id !== id) {
+        return item;
+      }
 
-            return coffee;
-        });
+      return coffee;
+    });
 
-        return this._coffees;
-    }
+    return this._coffees;
+  }
 
-    delete(id: number) {
-        this._coffees = this._coffees.filter(coffee => coffee.id !== id);
+  delete(id: number): Coffee[] {
+    this._coffees = this._coffees.filter((coffee) => coffee.id !== id);
 
-        return this._coffees;
-    }
+    return this._coffees;
+  }
 }
